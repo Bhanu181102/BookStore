7185543861197ce7c7c22ffb5562dc9ea914e4c0
Bhanu181102 <54906133+Bhanu181102@users.noreply.github.com>
2023-05-05 02:10:28 +0530

    Add files via upload

---
+++ src/main/java/com/BookStore/BookStore007Application.java
@@ -0,0 +1,13 @@
+package com.BookStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookStore007Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookStore007Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/BookStore/BookStore007ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BookStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStore007ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
